80b876557467ac37c41895e1c78045c4d74beb3e
theanmolsharma <anmolsharma0234@gmail.com>
2024-01-06 14:29:52 +0530

    feat: added docker-compose detup test environment and use regtest for testing
    Signed-off-by: theanmolsharma <anmolsharma0234@gmail.com>

--- test/wallet.spec.ts
+++ test/wallet.spec.ts
@@ -1,4 +1,5 @@
 import { EsploraClient, Wallet, WalletDB } from '../src/wallet';
+import * as fs from 'fs';
 
 describe('Wallet', () => {
     let wallet: Wallet;
@@ -11,9 +12,9 @@ describe('Wallet', () => {
         wallet = new Wallet({
             db: walletDB,
             networkClient: new EsploraClient({
-                protocol: 'https',
-                host: 'blockstream.info',
-                network: 'main',
+                protocol: 'http',
+                host: '127.0.0.1:8094',
+                network: 'regtest',
             }),
         });
     });
@@ -30,20 +31,21 @@ describe('Wallet', () => {
 
     it('should derive first receive address', async () => {
         const address = await wallet.deriveReceiveAddress();
-        expect(address).toBe('bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu');
+        expect(address).toBe('bcrt1qcr8te4kr609gcawutmrza0j4xv80jy8zeqchgx');
     });
 
     it('should derive second receive address', async () => {
         const address = await wallet.deriveReceiveAddress();
-        expect(address).toBe('bc1qnjg0jd8228aq7egyzacy8cys3knf9xvrerkf9g');
+        expect(address).toBe('bcrt1qnjg0jd8228aq7egyzacy8cys3knf9xvr3v5hfj');
     });
 
     it('should derive first change address', async () => {
         const address = await wallet.deriveChangeAddress();
-        expect(address).toBe('bc1q8c6fshw2dlwun7ekn9qwf37cu2rn755upcp6el');
+        expect(address).toBe('bcrt1q8c6fshw2dlwun7ekn9qwf37cu2rn755ufhry49');
     });
 
     afterAll(async () => {
         await wallet.close();
+        fs.rmSync('./test/wallet', { recursive: true, force: true });
     });
 });
