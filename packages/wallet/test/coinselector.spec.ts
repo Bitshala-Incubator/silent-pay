77172c61afb906395840e8b10e503b7193385554
Anmol Sharma <anmolsharma0234@gmail.com>
2024-03-03 12:12:08 +0530

    fix: update description in coin selector test
    Signed-off-by: Anmol Sharma <anmolsharma0234@gmail.com>

--- packages/wallet/test/coinselector.spec.ts
+++ packages/wallet/test/coinselector.spec.ts
@@ -21,7 +21,7 @@ describe('CoinSelector', () => {
 
     it.each([
         {
-            testName: 'should add change to the fee if it is dust',
+            description: 'add change to the fee if it is dust',
             testCoinValues: [
                 1005, 9040, 6440, 2340, 7540, 3920, 5705, 9030, 1092, 5009,
             ],
@@ -32,39 +32,42 @@ describe('CoinSelector', () => {
             expectedChange: 0, // Change will be zero when less than dust.
         },
         {
-            testName: 'change should be considered when greater than dust',
+            description: 'create change output if change is greater than dust',
             testCoinValues: [4000],
             txOutValue: 1000, // Less output so that the change is greater than dust.
             expectedChange: 2185, // expectedChange = totalSelectedCoin - (txOutValue + transactionFees).
         },
-    ])('%s', ({ txOutValue, testCoinValues, expectedChange }) => {
-        const coins: Coin[] = [];
+    ])(
+        'should $description',
+        ({ txOutValue, testCoinValues, expectedChange }) => {
+            const coins: Coin[] = [];
 
-        // Create coins based on the fixed array of coin values.
-        testCoinValues.forEach((value: number) => {
-            const coin = new Coin({ value });
-            coins.push(coin);
-        });
+            // Create coins based on the fixed array of coin values.
+            testCoinValues.forEach((value: number) => {
+                const coin = new Coin({ value });
+                coins.push(coin);
+            });
 
-        // Create a new Transaction
-        const transaction: Transaction = new Transaction();
+            // Create a new Transaction
+            const transaction: Transaction = new Transaction();
 
-        // Set the transaction output value with a random output script.
-        // Random 20 bytes and `0014${randomBytes.toString()}` will be a valid P2WPKH script.
-        transaction.addOutput(
-            Buffer.from(`0014${crypto.randomBytes(20).toString('hex')}`), // Ensure the first parameter passed to addOutput is a Buffer.
-            txOutValue,
-        );
+            // Set the transaction output value with a random output script.
+            // Random 20 bytes and `0014${randomBytes.toString()}` will be a valid P2WPKH script.
+            transaction.addOutput(
+                Buffer.from(`0014${crypto.randomBytes(20).toString('hex')}`), // Ensure the first parameter passed to addOutput is a Buffer.
+                txOutValue,
+            );
 
-        // Select coins and change using the CoinSelector.
-        const { coins: selectedCoins, change } = coinSelector.select(
-            coins,
-            transaction,
-        );
+            // Select coins and change using the CoinSelector.
+            const { coins: selectedCoins, change } = coinSelector.select(
+                coins,
+                transaction,
+            );
 
-        // Assert that the selected coins' size equals all available coins.
-        expect(selectedCoins.length).toBe(testCoinValues.length);
-        // Assert the change against the expected change condition.
-        expect(change).toBe(expectedChange);
-    });
+            // Assert that the selected coins' size equals all available coins.
+            expect(selectedCoins.length).toBe(testCoinValues.length);
+            // Assert the change against the expected change condition.
+            expect(change).toBe(expectedChange);
+        },
+    );
 });
