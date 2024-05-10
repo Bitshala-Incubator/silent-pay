a15e515eb68598057982b0caf25be5b8267890f9
emjshrx <emjshrx@gmail.com>
2024-05-10 13:00:20 +0530

    test: add tests

--- packages/core/test/transaction.spec.ts
+++ packages/core/test/transaction.spec.ts
@@ -48,4 +48,34 @@ describe('Transaction', () => {
             expect(transaction.locktime).toBe(tx.raw.locktime);
         },
     );
+
+    const complexTransactions = [
+        'P2PK',
+        'P2SH P2PK',
+        'P2SH P2PKH',
+        'P2WSH P2PK',
+        'P2WSH P2PKH',
+        'P2SH P2WSH P2PKH',
+        'P2SH P2WSH P2PK',
+        'Standard transaction (14:2)',
+        'Multisig',
+        'P2WSH Multisig',
+        'P2SH P2WSH Multisig',
+        'Coinbase transaction w/ witness',
+        'P2SH Multisig',
+        'Coinbase transaction',
+    ];
+
+    it.each(
+        validTransactions.filter(
+            (tx) => !complexTransactions.includes(tx.description),
+        ),
+    )('gets the Public Key from Inputs of a $description', (tx) => {
+        const transaction = Transaction.fromHex(tx.hex);
+        const receivedInPubkeys = tx.raw.ins.map((_, index) => {
+            const PK = transaction.getPublicKeyFromInput(index);
+            return PK ? PK?.toString('hex') : PK;
+        });
+        expect(receivedInPubkeys).toStrictEqual(tx.raw.inPubkeys);
+    });
 });
